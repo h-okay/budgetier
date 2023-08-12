be8aa3b8620697d3dad46fadbb4778b1e02c62aa
Hakan Okay <hokay18@gmail.com>
2023-08-11 21:34:13 -0400

    Initial Commit

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,20 +3,75 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Category;
 use Illuminate\Database\Seeder;
+
+
 
 class DatabaseSeeder extends Seeder
 {
+    private static $DEFAULT_CATEGORIES = [
+        'Home',
+        'Daily Living',
+        'Transportation',
+        'Entertainment',
+        'Health',
+        'Vacation',
+        'Personal',
+        'Financial Obligations'
+    ];
+
+    private static $DEFAULT_SUBCATEGORIES = [
+        'Rent' => 'Home',
+        'Home Insurance' => 'Home',
+        'Repairs' => 'Home',
+        'Services' => 'Home',
+        'Utilities' => 'Home',
+        'Groceries' => 'Daily Living',
+        'Dining Out' => 'Daily Living',
+        'Alcohol' => 'Daily Living',
+        'Cigarattes' => 'Daily Living',
+        'Uber/Lyft' => 'Transportation',
+        'Public Transportation' => 'Transportation',
+        'Subscriptions' => 'Entertainment',
+        'Concerts/Shows' => 'Entertainment',
+        'Movies' => 'Entertainment',
+        'Health Insurance' => 'Health',
+        'Prescriptions' => 'Health',
+        'Over the Counter Drugs' => 'Health',
+        'Airfare' => 'Vacation',
+        'Accomodations' => 'Vacation',
+        'Food' => 'Vacation',
+        'Souvenirs' => 'Vacation',
+        'Rentals' => 'Vacation',
+        'Activities' => 'Vacation',
+        'Clothing' => 'Personal',
+        'Books' => 'Personal',
+        'Home' => 'Personal',
+        'Tech' => 'Personal',
+        'Education' => 'Personal',
+        'Gifts' => 'Personal',
+        'Credit Card' => 'Financial Obligations',
+        'Student Loans' => 'Financial Obligations',
+        'Mortgage' => 'Financial Obligations',
+        'Car Loan' => 'Financial Obligations',
+    ];
+
     /**
      * Seed the application's database.
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        foreach ($this::$DEFAULT_CATEGORIES as $category) {
+            Category::create([
+                'name' => $category
+            ]);
+        }
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        // foreach ($this::$DEFAULT_SUBCATEGORIES as $subcategory) {
+        //     SubCategory::create([
+        //         'name' => $subcategory
+        //     ]);
+        // }
     }
 }
